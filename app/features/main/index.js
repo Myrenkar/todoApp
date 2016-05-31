9551da97cfe0437e865de794c92b7f03f221393f
myrenkar <piotr.torczynski@gmail.com>
2016-05-31 17:50:55 +0000

    nie dziala file-loader

--- app/features/main/index.js
+++ app/features/main/index.js
@@ -3,10 +3,15 @@ import uirouter from 'angular-ui-router';
 
 import routing from './main.routes';
 import MainController from './main.controller';
-import angularfire from 'angularfire'
-import firebase from 'firebase'
+import angularfire from 'angularfire';
+import firebase from 'firebase';
+
+// import login from './directives/login';
+import todos from './directives/todos';
 
 export default angular.module('app.main', [uirouter, angularfire])
   .config(routing)
   .controller('MainController', MainController)
+  .directive('login', login)
+  .directive('todos', todos)
   .name;
--- app/features/main/main.controller.js
+++ app/features/main/main.controller.js
@@ -1,7 +1,26 @@
+/*global Firebase*/
+
 export default class MainController {
-  constructor($scope, $firebaseArray ) {
-      
-      var  ref = new Firebase("https://learn11.firebaseio.com");
-      $scope.messages = $firebaseArray (ref);
-  }
+    constructor($scope, $firebaseArray) {
+
+        var ref = new Firebase("https://learn11.firebaseio.com");
+        $scope.messages = $firebaseArray(ref);
+
+        $scope.addMessage = function() {
+            $scope.messages.$add({
+                text: $scope.newMessageText
+            });
+        };
+    };
+    
+    login($scope, $firebaseAuth) {
+         var ref = new Firebase("https://learn11.firebaseio.com");
+        var auth = $firebaseAuth(ref);
+        // login with Facebook
+        auth.$authWithOAuthPopup("facebook").then(function(authData) {
+            console.log("Logged in as:", authData.uid);
+        }).catch(function(error) {
+            console.log("Authentication failed:", error);
+        });
+    };
 }
