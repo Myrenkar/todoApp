e40571e8c282a14529fd29e07710f6ad9e6c60e5
myrenkar <piotr.torczynski@gmail.com>
2016-06-03 18:51:12 +0000

    Cos tam dziala wiecej:

--- app/features/main/main.controller.js
+++ app/features/main/main.controller.js
@@ -4,35 +4,68 @@ export default class MainController {
     constructor($scope, $firebaseArray, $firebaseAuth) {
 
         var ref = new Firebase("https://learn11.firebaseio.com/todos");
-        ref.orderByChild('mail').equalTo($scope.email)
         $scope.messages = $firebaseArray(ref);
 
+        $scope.tasks = [];
+        $scope.editIndex = false;
+        console.log("  messages " + $scope.messages)
         $scope.addMessage = function() {
             $scope.messages.$add({
                 text: $scope.newMessageText
             });
         };
 
-
+        $scope.isLogged = false
+        console.log("  Poczatek " + $scope.isLogged)
         $scope.loginUser = function() {
             ref.authWithPassword({
                 email: $scope.email,
                 password: $scope.password
             }, function(error, authData) {
                 if (error) {
+                    $scope.isLogged = false
                     console.log("Login Failed!", error);
                 }
                 else {
-                    console.log("Authenticated successfully with payload:", authData);
+                    $scope.isLogged = true
+                    console.log($scope.isLogged)
+                    console.log("Authenticated successfully with payload:", $scope.email);
                 }
             });
+            console.log('https://samplechat.firebaseio-demo.com/users/$scope.messages[index].id')
+            console.log("  Koniec " + $scope.isLogged)
         };
 
-        $scope.addTask = function(){
+        $scope.addTask = function() {
             var message_ref = new Firebase('https://learn11.firebaseio.com/todos');
             var newMessageRef = message_ref.push();
-        newMessageRef.set({ 'done': 'false', 'text': $scope.task, 'mail' : $scope.email });
-            
+            newMessageRef.set({
+                'done': true,
+                'text': $scope.task,
+                'mail': $scope.email
+            });
+
         };
+        $scope.editTask = function(index) {
+            $scope.task = $scope.messages[index].text;
+            console.log($scope.messages[index].text);
+            $scope.editIndex = index;
+        }
+
+        $scope.doneTask = function(index) {
+            $scope.messages[index].done = true;
+
+        }
+        $scope.unDoneTask = function(index) {
+            $scope.messages[index].done = false;
+        }
+        console.log("  Daleko " + $scope.isLogged)
+
+        $scope.deleteTask = function(index) {
+            
+            var fredRef = new Firebase('https://samplechat.firebaseio-demo.com/users/$scope.messages[index].id');
+           
+        }
+
     }
 }
